497fa9d1dd0ab4ddd5d160e91b6e1028e1a3ff97
Daniele Tenga <cookiie@Danieles-MacBook-Air-2.local>
2020-07-13 01:44:47 -0400

    first commit

---
+++ server/build/action_page.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+
+    <?php echo $_POST["name"]; ?><br>
+    <?php echo $_POST["Email"]; ?><br>
+    <?php echo $_POST["message"]; ?><br>
+
+</body>
+</html>
---
+++ server/build/precache-manifest.6a284e6c3be0c796706fb2271ee29819.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2857e9ec64de7d0b6e39d58233cd8049",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7d253d3b6bcc04778b5f",
+    "url": "/static/css/main.0430f15b.chunk.css"
+  },
+  {
+    "revision": "bf79e0fc48d59fd198f7",
+    "url": "/static/js/2.4c736102.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4c736102.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d253d3b6bcc04778b5f",
+    "url": "/static/js/main.f738bb94.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
